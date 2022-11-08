336f005402a5102c05bafcec9fcc5c2b353b2333
Buyoung Chung <sunarol@naver.com>
2022-11-08 18:12:13 +0900

    test: Comparator reverse test

--- __test__/Comparator.test.js
+++ __test__/Comparator.test.js
@@ -4,6 +4,15 @@ const comparator = new Comparator();
 
 describe("Comparator Test", () => {
   test("defaultCompareFunction Test", () => {
-    expect(comparator.compare(1, 2)).toBeFalsy();
+    expect(comparator.compare(1, 1)).toBe(0);
+    expect(comparator.compare(1, 2)).toBe(-1);
+    expect(comparator.compare(2, 1)).toBe(1);
+  });
+
+  test("reverse Test", () => {
+    comparator.reverse();
+    expect(comparator.compare(1, 1)).toBe(0);
+    expect(comparator.compare(1, 2)).toBe(1);
+    expect(comparator.compare(2, 1)).toBe(-1);
   });
 });
